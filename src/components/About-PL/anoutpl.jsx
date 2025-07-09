f4702dc21a3fac7772002f2df35ff4ead72abd1b
harshalsonune55 <harshalsonune55@gmail.com>
2025-07-09 15:53:26 +0530

    font changes are made

--- src/components/About-PL/anoutpl.jsx
+++ src/components/About-PL/anoutpl.jsx
@@ -17,10 +17,10 @@ export default function AboutDaxx() {
         <p className="text-green-700 font-semibold text-sm tracking-wider uppercase relative inline-block mb-4 before:content-[''] before:absolute before:left-[-40px] before:top-1/2 before:w-8 before:h-[1px] before:bg-orange-500 after:content-[''] after:absolute after:right-[-40px] after:top-1/2 after:w-8 after:h-[1px] after:bg-orange-500 ">
           About PrinceLand Developers
         </p>
-        <h2 className="text-4xl md:text-5xl font-bold text-gray-800 leading-tight go">
+        <h2 className="text-4xl md:text-5xl font-bold text-gray-800 leading-tight go text-left">
         Princelands Developers is a trusted name in Nagpur’s land and plot development
         </h2>
-        <p className="mt-6 text-lg text-gray-600 max-w-3xl mx-auto leading-relaxed">
+        <p className="mt-6 text-lg text-gray-600 max-w-3xl mx-auto leading-relaxed text-left">
         Our focus is clear, to offer legally secure, NMRDA-sanctioned, RERA-registered, ready-to-construct plots backed by transparency, professional service, and long-term value.
 We work with a client-first approach, delivering developments that are not just investment opportunities but foundations for future homes and lasting relationships.
 
